d8e73c0333ead6aaf3016cbe3da4e7605ee3451a
Min Kim <minkimcello@gmail.com>
2025-02-09 15:39:11 -0500

    Create a backend plugin with a scheduler

---
+++ backstage/plugins/github-code-scanning-backend/src/index.ts
@@ -0,0 +1,1 @@
+export { githubCodeScanningPlugin as default } from './plugin';
---
+++ backstage/plugins/github-code-scanning-backend/src/plugin.ts
@@ -0,0 +1,30 @@
+import {
+  coreServices,
+  createBackendPlugin,
+} from '@backstage/backend-plugin-api';
+
+/**
+ * githubCodeScanningPlugin backend plugin
+ *
+ * @public
+ */
+
+export const githubCodeScanningPlugin = createBackendPlugin({
+  pluginId: 'github-code-scanning',
+  register(env) {
+    env.registerInit({
+      deps: {
+        scheduler: coreServices.scheduler,
+        config: coreServices.rootConfig,
+      },
+      async init({ scheduler }) {
+        await scheduler.scheduleTask({
+          frequency: { seconds: 3 },
+          timeout: { seconds: 20 },
+          id: 'github-reports',
+          fn: async () => { console.log("hello") },
+        })
+      },
+    });
+  },
+});
